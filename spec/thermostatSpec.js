f9c0bc8a2011e060fdfeb593116d2725a3220a33
uburger <ursburger@gmail.com>
2021-11-17 16:08:58 +0100

    up funtion

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,9 @@ describe('Thermostat', function(){
   it('has a default temperature of 20', function() {
     expect(thermostat.temperature).toEqual(20)
   });
+
+  it('increases teamperature', function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21)
+  });
 });
